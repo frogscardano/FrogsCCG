a29a21892d76806fb195afee93ed65c8416a12c2
frogscardano <frogscardano@gmail.com>
2025-05-28 05:24:17 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,15 +5,7 @@ const nextConfig = {
     domains: ['ipfs.io', 'gateway.ipfs.io', 'dweb.link', 'pool.pm'],
     unoptimized: true
   },
-  // Optimize bundle size
-  webpack: (config) => {
-    config.optimization.minimize = true;
-    return config;
-  },
-  // Add trailing slashes to help with routing
   trailingSlash: true,
-  // Ensure static exports work properly
-  output: 'standalone',
 }
 
 module.exports = nextConfig
