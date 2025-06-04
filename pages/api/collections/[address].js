a98ac0e7ac3417aa2c40739c72b65f25088e28ff
frogscardano <frogscardano@gmail.com>
2025-06-04 20:47:41 +0300

    Update [address].js add ...db

--- pages/api/collections/[address].js
+++ pages/api/collections/[address].js
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { prisma } from '../../utils/db.js';
 import { getFrogStats } from '../../../utils/frogData';
 import { v4 as uuid4 } from 'uuid';
 
