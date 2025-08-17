e1d7fe3f42badd7afd2ef16c3c0c2d0eb04096bb
frogscardano <frogscardano@gmail.com>
2025-08-17 16:13:14 +0300

    Update cardanoTransactions.js

--- pages/api/cardanoTransactions.js
+++ pages/api/cardanoTransactions.js
@@ -2,7 +2,7 @@
 // Handles Cardano transactions for NFT pack openings
 
 import { BlockFrostAPI } from '@blockfrost/blockfrost-js';
-import { prisma } from '../../utils/db.js';
+import { prisma } from '../utils/db';
 
 // Initialize Blockfrost API for Cardano
 const initializeBlockfrostBackend = (projectId, network = 'mainnet') => {
@@ -220,7 +220,7 @@ const recordPackOpening = async (paymentId, userId, walletAddress, txHash) => {
     // Get available NFTs for this pack type
     // In a real implementation, you would have logic to determine which NFTs are available
     // Here's a simplified version that gets a random sample of NFTs from the database
-    const availableNFTs = await prisma.NFT.findMany({
+    const availableNFTs = await prisma.nFT.findMany({
       where: {
         packOpeningId: null, // Only get NFTs that haven't been claimed yet
         // You might also filter by other properties specific to this pack
@@ -250,10 +250,10 @@ const recordPackOpening = async (paymentId, userId, walletAddress, txHash) => {
 
     // Update the selected NFTs to be associated with this pack opening
     for (const nft of selectedNFTs) {
-      await prisma.NFT.update({
+      await prisma.nFT.update({
         where: { id: nft.id },
         data: {
-          userId: userId,
+          ownerId: userId,
           packOpeningId: packOpening.id,
           claimedAt: new Date()
         }
